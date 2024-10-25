6af6a05cfc33e7a69194e51b5359f27bf416955f
Ilia Markovski <i.markovski@jdm.de>
2024-10-25 11:50:59 +0300

    Add new var

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ function App() {
   return (
     <div className="max-w-7xl px-5 mx-auto">
       <h1>{import.meta.env.VITE_ENVIRONMENT}</h1>
+      <h2>This is {import.meta.env.VITE_TEST}</h2>
     </div>
   )
 }

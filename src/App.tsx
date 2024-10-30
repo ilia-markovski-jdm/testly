b4f2d9bd33a949e3d9a914e9d0b6aaa595dcc8b8
Ilia Markovski <i.markovski@jdm.de>
2024-10-30 11:48:17 +0200

    add description

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ function App() {
     <div className="max-w-7xl px-5 mx-auto">
       <h1>{import.meta.env.VITE_ENVIRONMENT}</h1>
       <h2>This is {import.meta.env.VITE_TEST}</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero perspiciatis, quos fugit rerum ut vero ratione delectus quas eaque esse deleniti et! Necessitatibus corporis iste eius veniam voluptates, eaque minus?</p>
     </div>
   )
 }
